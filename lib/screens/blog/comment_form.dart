44e44a9edbe7b0a97c21b2bd85a0066b81b7004a
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-01-30 23:00:45 +0800

    feat: add reusable styled filled button widget

--- lib/screens/blog/comment_form.dart
+++ lib/screens/blog/comment_form.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simple_blog_flutter/screens/login/login_screen.dart';
+import 'package:simple_blog_flutter/shared/styled_button.dart';
 import 'package:simple_blog_flutter/shared/styled_text.dart';
-import 'package:simple_blog_flutter/theme.dart';
 
 class CommentForm extends StatelessWidget {
   const CommentForm({super.key});
@@ -14,21 +14,14 @@ class CommentForm extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             StyledText('Login to leave a comment'),
-            FilledButton(
+            StyledFilledButton(
+              'Login',
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => LoginScreen()),
                 );
               },
-              style: FilledButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                backgroundColor: AppColors.primary.withValues(alpha: 0.9),
-              ),
-              child: StyledText('Login'),
             ),
           ],
         ),
---
+++ lib/shared/styled_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:simple_blog_flutter/shared/styled_text.dart';
+import 'package:simple_blog_flutter/theme.dart';
+
+class StyledFilledButton extends StatelessWidget {
+  const StyledFilledButton(
+    this.text, {
+    super.key,
+    required this.onPressed,
+    this.color,
+  });
+
+  final String text;
+  final void Function() onPressed;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: onPressed,
+      style: FilledButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        backgroundColor: color ?? AppColors.primary,
+      ),
+      child: StyledText(text),
+    );
+  }
+}
