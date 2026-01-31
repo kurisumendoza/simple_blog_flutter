88c36020077056f75e86b24f10259706e028ecf1
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-01-31 08:31:34 +0800

    feat: add user data and username getters

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -2,4 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DatabaseService {
   static final supabase = Supabase.instance.client;
+
+  static User? get user => supabase.auth.currentUser;
+
+  static String? get userName => user?.userMetadata?['user'];
 }
