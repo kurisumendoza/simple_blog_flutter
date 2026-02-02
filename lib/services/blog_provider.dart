463252fdef349d60c474de6cf0b4a924acc01e78
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-02-02 11:41:56 +0800

    feat: add BlogProvider and refactor for error handling

---
+++ lib/services/blog_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:simple_blog_flutter/models/blog.dart';
+import 'package:simple_blog_flutter/services/blog_service.dart';
+
+class BlogProvider extends ChangeNotifier {
+  List<Blog> _blogs = [];
+  List<Blog> get blogs => _blogs;
+
+  Future<bool> getBlogs(int start, int end) async {
+    try {
+      _blogs = await BlogService.getBlogs(start, end);
+      notifyListeners();
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<int> getBlogsCount() async {
+    try {
+      final count = await BlogService.getBlogsCount();
+
+      return count;
+    } catch (e) {
+      return -1;
+    }
+  }
+}
--- lib/services/blog_service.dart
+++ lib/services/blog_service.dart
@@ -1,22 +1,33 @@
 import 'package:simple_blog_flutter/models/blog.dart';
 import 'package:simple_blog_flutter/services/database_service.dart';
 
-class BlogService extends DatabaseService {
+class BlogService {
   // get blogs per page
   static Future<List<Blog>> getBlogs(int start, int end) async {
-    final data = await DatabaseService.supabase
-        .from('blogs')
-        .select()
-        .order('created_at')
-        .range(start, end);
+    try {
+      final data = await DatabaseService.supabase
+          .from('blogs')
+          .select()
+          .order('created_at')
+          .range(start, end);
 
-    return data.map<Blog>((d) => Blog.fromSupabase(d)).toList();
+      return data.map<Blog>((d) => Blog.fromSupabase(d)).toList();
+    } catch (e) {
+      rethrow;
+    }
   }
 
   // get blogs total count
   static Future<int> getBlogsCount() async {
-    final data = await DatabaseService.supabase.from('blogs').select().count();
+    try {
+      final data = await DatabaseService.supabase
+          .from('blogs')
+          .select()
+          .count();
 
-    return data.count;
+      return data.count;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
