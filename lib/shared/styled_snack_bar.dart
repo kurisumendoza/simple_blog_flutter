9d0aaf3e420b01f93d6bb22e8dff3aed790316ab
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-02-02 10:29:21 +0800

    feat: add reusable custom SnackBar widget

---
+++ lib/shared/styled_snack_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:simple_blog_flutter/shared/styled_text.dart';
+import 'package:simple_blog_flutter/theme.dart';
+
+SnackBar styledSnackBar({required String message, bool isError = false}) {
+  return SnackBar(
+    duration: const Duration(seconds: 2),
+    behavior: SnackBarBehavior.floating,
+    margin: const EdgeInsets.all(16),
+    backgroundColor: isError ? Colors.red[400] : AppColors.accent,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    content: StyledText(message),
+    action: SnackBarAction(
+      label: 'OK',
+      textColor: AppColors.text,
+      onPressed: () {},
+    ),
+  );
+}
