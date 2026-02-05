a15b2c7ec1d5a3ddf13a3dd38df2d8e05c761fda
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-02-05 11:27:58 +0800

    feat: add profile storage service

--- lib/services/profile_storage_service.dart
+++ lib/services/profile_storage_service.dart
@@ -1,1 +1,32 @@
-class ProfileStorageService {}
+import 'dart:io';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:simple_blog_flutter/services/database_service.dart';
+
+class ProfileStorageService {
+  // get image URL
+  static String getImageUrl(String imagePath) {
+    final String publicUrl = DatabaseService.supabase.storage
+        .from('profile-images')
+        .getPublicUrl(imagePath);
+
+    return publicUrl;
+  }
+
+  // insert new image
+  static Future<void> addImage(String fileName, File file) async {
+    await DatabaseService.supabase.storage
+        .from('profile-images')
+        .upload(
+          fileName,
+          file,
+          fileOptions: const FileOptions(cacheControl: '3600', upsert: false),
+        );
+  }
+
+  // delete one image
+  static Future<void> deleteImage(String filePath) async {
+    await DatabaseService.supabase.storage.from('profile-images').remove([
+      filePath.trim(),
+    ]);
+  }
+}
